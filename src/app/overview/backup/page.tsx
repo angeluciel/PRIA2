e3c3531dd793a60fd781e85d5f9cbe3d64f8336d
Joao-AZ <jpmizidoro@gmail.com>
2025-12-05 20:55:54 +0000

    tweaking with the animation of expanding

--- src/app/overview/backup/page.tsx
+++ src/app/overview/backup/page.tsx
@@ -1,13 +1,18 @@
+"use client";
 import StatusCard, { IJob } from "@/components/overview/statusCard";
 import StatusHeader from "@/components/overview/header";
+import { useState } from "react";
 
 export default function backupPage() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const jobList: IJob[] = [
     {
       id: "abc",
       name: "Backup Job 1 | WV | DB",
       status: "success",
       lastExecution: "10/12/2025",
+      type: "hyper-V backup",
     },
   ];
   return (
@@ -16,11 +21,12 @@ export default function backupPage() {
       <div className='px-8 py-8 grid grid-cols-3 gap-4'>
         <StatusCard
           company='Dinamio'
-          status='warning'
+          status='error'
           subtitle='Backups estão ok'
           jobCount={4}
           jobs={jobList}
-          isExpanded
+          isExpanded={isExpanded}
+          onToggle={() => setIsExpanded(!isExpanded)}
         />
       </div>
     </div>
--- src/components/overview/statusCard.tsx
+++ src/components/overview/statusCard.tsx
@@ -5,7 +5,9 @@ import {
   CircleCheckBig,
   CircleX,
   Dot,
+  Server,
 } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
 
 export interface IJob {
   id: string;
@@ -31,6 +33,8 @@ export default function StatusCard({
   subtitle,
   jobCount,
   isExpanded,
+  jobs,
+  onToggle,
 }: StatusCardProps) {
   const statusColor = {
     success:
@@ -43,22 +47,28 @@ export default function StatusCard({
 
   return (
     <div
-      className={`flex flex-col ${statusColor} border transition-all duration-300 ease-out rounded-lg overflow-hidden`}
+      className={`flex flex-col ${statusColor} border transition-all duration-300 ease-out rounded-lg overflow-hidden font-geist`}
     >
       {}
       <div
         className={clsx(
           `max-w-[42rem] flex items-center justify-start gap-4 px-8 py-6`,
-          isExpanded ? `border-b ${statusColor}` : ""
+          isExpanded ? `border-b ${statusColor}` : ``
         )}
+        onClick={onToggle}
       >
-        <ChevronRight />
+        <ChevronRight
+          className={clsx(
+            `transition-transform duration-200 ease-out`,
+            isExpanded && "rotate-90"
+          )}
+        />
         <div>
           {status === "warning" && <AlertTriangle />}
           {status === "success" && <CircleCheckBig />}
           {status === "error" && <CircleX />}
         </div>
-        <div className='flex gap-16 font-geist'>
+        <div className='flex gap-16'>
           <div className='flex flex-col items-start'>
             <h2 className='font-semibold text-zinc-800 tracking-wide'>
               {company}
@@ -71,11 +81,38 @@ export default function StatusCard({
           </div>
         </div>
       </div>
-      <div className='text-foreground-error'>
-        <div className=''>
-          <div>aaaa</div>
-        </div>
-      </div>
+      <AnimatePresence>
+        {isExpanded && (
+          <motion.div
+            initial={{ height: -4, opacity: 0 }}
+            animate={{ height: 320, opacity: 1 }}
+            exit={{ opacity: 0.3 }}
+            className='flex flex-col items-center justify-start h-80 overflow-y-auto overflow-x-hidden p-4'
+          >
+            {jobs.map((job) => (
+              <div
+                className={`flex justify-between items-center px-4 py-2 w-full border ${statusColor} !bg-black/5 rounded-sm`}
+              >
+                <div className='flex gap-3 items-center'>
+                  <Server />
+                  <div className='flex flex-col items-start'>
+                    <h3 className='text-zinc-900 font-semibold'>{job.name}</h3>
+                    {job.type && (
+                      <span className='text-sm font-semibold'>{job.type}</span>
+                    )}
+                  </div>
+                </div>
+                <div className='flex flex-col justify-between items-end'>
+                  <span className='text-sm'>
+                    Last execution: {job.lastExecution}
+                  </span>
+                  <span className={`font-semibold`}>Status: {job.status}</span>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
