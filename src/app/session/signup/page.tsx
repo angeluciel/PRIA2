0a69944dba365582097b5cbde1703d65a778967d
Joao-AZ <jpmizidoro@gmail.com>
2025-12-16 17:30:06 +0000

    idk

--- src/app/session/signup/page.tsx
+++ src/app/session/signup/page.tsx
@@ -4,7 +4,7 @@ export default function signup() {
   return (
     <div className='flex-center  w-dvw min-h-dvh bg-background text-foreground'>
       <Link
-        href={`/overview`}
+        href={`/overview/backup`}
         className='py-2 px-3 rounded-lg border border-foreground hover:bg-foreground/10'
       >
         Dashboard
